2e123a1115e0c0d6aa630493b50410f2de3b9f7c
Ucokkici <akbar15kece@gmail.com>
2025-08-05 06:52:05 +0700

    Fix import path case sensitivity for Contact.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import Navbar from "./components/Navbar";
-import Hero from "./components/hero";
+import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
-import Contact from "./components/contact";
+import Contact from "./components/Contact";
 
 function App() {
   return (
